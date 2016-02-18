d6f50fb45224326e7a8a3844119d637cb4ea7345
Carson Trent Long <ctlong.970@gmail.com>
2016-02-18 17:26:54 +0800

    fixed typo

--- app/models/seafood.rb
+++ app/models/seafood.rb
@@ -2,8 +2,8 @@ class Seafood < ActiveRecord::Base
   belongs_to :species
   belongs_to :city
 
-  validates :name,                 presence: true, length: { is: 2 }
-  validates :source_location,      presence: true, length: { maximum: 200 }
+  validates :name,                 presence: true, length: { maximum: 200 }
+  validates :source_location,      presence: true, length: { is: 2 }
   validates :sustainability_level, presence: true, inclusion: { in: %w(green yellow red), message: "%w{value} is not a valid sustainability level" }
   validates :harvesting_method,    presence: true, inclusion: { in: %w(farmed wild), message: "%w{value} is not a valid harvesting method" }
   validates :facts,                presence: true
